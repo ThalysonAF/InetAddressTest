ac127adaed692c48563a0c4739c5d3723fcc1839
Ubuntu <ubuntu@ip-172-31-66-95.ec2.internal>
2020-11-03 21:46:34 +0000

    Testes e adição do método getHostAddress

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -11,11 +11,14 @@ public class App
     public static void main( String[] args )
     {
         try{
-            
+            //Pegando o domínio do servidor
             String name = "www.ufs.br";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
-            System.out.println( "Address:   "+ address);
+            //Pegar somente o ip
+            System.out.println( "Address:   "+ address.getHostAddress());
+            //Nome e IP
+            //System.out.println( "Address:   "+ address);
 
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
